eeb413af70aaec33324f6f0851bc9916bcc1669a
João Pedro <joao.guadalupe06@gmail.com>
2025-10-29 21:05:12 -0300

    debug 2

--- area_loss_fn.py
+++ area_loss_fn.py
@@ -7,6 +7,10 @@ class WeightedMSELoss(nn.Module):
         super().__init__()
 
     def forward(self, predictions, targets, weights):
+        if predictions.dim() == 4 and predictions.shape[1] > 1:
+            predictions = predictions.mean(dim=1, keepdim=True) 
+        if predictions.shape[-2:] != targets.shape[-2:]:
+            predictions = F.interpolate(predictions, size=targets.shape[-2:], mode='bilinear', align_corners=False)
         raw_loss = F.mse_loss(predictions, targets, reduction='none')
         weighted_loss = raw_loss * weights
         area_loss_sum = torch.sum(weighted_loss)
