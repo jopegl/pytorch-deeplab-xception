fae62e1e730e5c56f5a192ddb241a7eab5b188d0
João Pedro <joao.guadalupe06@gmail.com>
2025-11-19 13:08:06 -0300

    clean

--- area_loss_fn.py
+++ area_loss_fn.py
@@ -7,24 +7,3 @@ def masked_mse_loss(pred, targets, mask):
     masked_difference = difference * mask
     loss = masked_difference.sum() / mask.sum()
     return loss
-
-class WeightedMSELoss(nn.Module):
-    def __init__(self):
-        super().__init__()
-
-    def forward(self, predictions, targets, weights):
-        if predictions.dim() == 4 and predictions.shape[1] > 1:
-            predictions = predictions.mean(dim=1, keepdim=True) 
-        if predictions.shape[-2:] != targets.shape[-2:]:
-            predictions = F.interpolate(predictions, size=targets.shape[-2:], mode='bilinear', align_corners=False)
-        if targets.ndim == 3:
-            targets = targets.unsqueeze(1)
-
-        #perguntar sobre o produto de hadamaard, não estou entendendo como usar os pesos
-        raw_loss = F.mse_loss(predictions, targets, reduction='none')
-        weighted_loss = raw_loss * weights
-        area_loss_sum = torch.sum(weighted_loss)
-        weight_sum = torch.sum(weights)
-        avg_loss = area_loss_sum/weight_sum
-        return avg_loss
-    
--- testes.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import math
-import os
-
-caminho = 'leaf\masks\leaf_001_002_segmentation.raw'
-
-tamanho_bytes = os.path.getsize(caminho)
-lado = int(math.sqrt(tamanho_bytes))  # assumindo imagem quadrada
-print(f'A imagem é aproximadamente {lado}x{lado}')
